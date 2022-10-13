7404bc15bbc9d0f7a35849734a627ffd8eed6ce4
mporras77 <mporras77@misena.edu.co>
2022-10-13 09:51:12 -0500

    proyecto_Final

--- PROYECTo/responsive-navbar-yt-master/slider.js
+++ PROYECTo/responsive-navbar-yt-master/slider.js
@@ -9,18 +9,18 @@ slider. insertAdjacentElement('afterbegin', sliderSectionLast);
 
 function Next(){
     let sliderSectionFirst = document.querySelectorAll(".slider__section") [0];
-    slider.style.marginLeft ="0";
+    slider.style.marginLeft ="-200%";
     slider.style.transition ="all 0.5s";
     setTimeout(function(){
-    slider.style.transition = "none";
-    slider. insertAdjacentElement('beforeend', sliderSectionFirst) ;
-    slider.style.marginLeft = "-100%";
-    }, 500);
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst) ;
+        slider.style.marginLeft = "-100%";
+        }, 500);
 }
 function Prev(){
     let sliderSection = document.querySelectorAll(".slider__section") 
     let sliderSectionLast = sliderSection[sliderSection. length -1];
-    slider.style.marginLeft ="-200%";
+    slider.style.marginLeft ="0";
     slider.style.transition ="all 0.5s";
     setTimeout(function(){
     slider.style.transition = "none";
@@ -34,14 +34,11 @@ btnRight.addEventListener('click', function(){
     Next();
 });
 
-btnRight.addEventListener('click', function(){
+btnLeft.addEventListener('click', function(){
     Prev();
 });
 
+setInterval(function(){
+    Next();
+}, 5000);
  
- 
-
-
-
-
-
